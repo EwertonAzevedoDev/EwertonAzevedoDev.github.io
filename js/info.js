79cb6cce9eee6747258a56715e588c9668c1bdf9
EwertonAzevedoDev <ewerton.azevedo.dev@gmail.com>
2021-12-31 17:01:59 -0300

    correcoes

--- js/info.js
+++ js/info.js
@@ -3,13 +3,13 @@ let experiencias = [
         "empresa": "BPA Technologies",
         "cargo": "Desenvolvedor RPA",
         "periodo": "(09/2020 - 09/2021)",
-        "descricao":"Desenvolvi automações que acessavam planilhas, sites, programas de terceiros e fazia o dashboards dessass automações para acompanhamento do desenpenho da automação, ensinei colegas iniciantes como utilizar algumas ferramentas da empresa e auxiliei no desenvolvimento de varias automações."
+        "descricao":"Desenvolvi automações que acessavam planilhas, sites, programas de terceiros e fazia o dashboards dessas automações para acompanhamento do desempenho da automação, ensinei colegas iniciantes como utilizar algumas ferramentas da empresa e auxiliei no desenvolvimento de várias automações."
     },
     {
         "empresa": "Yellow Lamp",
         "cargo": "Desenvolvedor back-end",
         "periodo": "(08/2019 - 03/2020)",
-        "descricao":"Participei de alguns projetos já em andamento, tais como, sistema de delivery, sistema de loja virtual e sistemas mais avançados utilizando API de terceiros. Participei do desenvolvimento de um projeto grande de pesquisa e avaliação de restaurantes onde fiquei responsavel integralmente pelo back-end desenvolvido em PHP utilizando o framework Laravel. E desenvolvi uma página web com painel administrativo para inserção de conteúdo dinâmico."
+        "descricao":"Participei de alguns projetos já em andamento, tais como, sistema de entrega, sistema de loja virtual e sistemas mais avançados utilizando API de terceiros. Participei do desenvolvimento de um projeto grande de pesquisa e avaliação de restaurantes onde fiquei responsável integralmente pelo back-end desenvolvido em PHP utilizando o framework Laravel. E desenvolvi uma página web com painel administrativo para inserção de conteúdo dinâmico."
     },
     {
         "empresa": "SVMA",
@@ -18,7 +18,7 @@ let experiencias = [
         "descricao":"Durante o período estagiado utilizei o Linux para a configuração de um ambiente Web. Tive contato com o método Scrum e a plataforma Trello para organização de projetos, realizei estudos dos frameworks Drupal e Wordpress e implementei o sistema GLPI no servidor de testes para homologação do mesmo."
     }     
 ]
-
+console.table(experiencias)
 let jobs = document.getElementById("jobs")
 
 experiencias.forEach((element) =>{
