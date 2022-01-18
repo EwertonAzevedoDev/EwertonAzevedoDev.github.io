43af77f5120fea43ae1d5a3e922ad4621df23daa
EwertonAzevedoDev <ewerton.azevedo.dev@gmail.com>
2022-01-17 21:48:56 -0300

    filter

--- js/filter.js
+++ js/filter.js
@@ -1,22 +1,20 @@
 // init Isotope
-var elem = document.querySelector('.grid');
-var $grid = new Isotope( elem, {
-  // options
-  itemSelector: '.element-item',
-  layoutMode: 'fitRows'
+var $grid = $('.grid').isotope({
+    itemSelector: '.element-item',
+    layoutMode: 'fitRows'    
 });
 
 // bind filter button click
-$('.filters-button-group').on( 'click', 'li', function() {
-    var filterValue = $( this ).attr('data-filter');    
-    $grid.isotope({ filter: filterValue });
+$('.filters-button-group .switch').click(function() {    
+    
+    $('.filters-button-group .switch').removeClass('is-checked');
+    $(this).addClass('is-checked');
+
+    var selector = $(this).attr('data-filter');
+    console.log("filtering by: " + selector)
+    $grid.isotope({
+        filter: selector
+    });
+    return false;
 });
 
-// change is-checked class on buttons
-$('.button-group').each( function( i, buttonGroup ) {
-    var $buttonGroup = $( buttonGroup );
-    $buttonGroup.on( 'click', 'li', function() {
-        $buttonGroup.find('.is-checked').removeClass('is-checked');
-        $( this ).addClass('is-checked');
-    });
-});
